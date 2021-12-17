c15fb22fe082f340dc0d8f9ce57cbd7c8dadbce4
om Vivek <74807404+omvivek@users.noreply.github.com>
2021-12-17 16:30:07 +0530

    Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -110,18 +110,13 @@ class Page extends StatelessWidget {
             _scafoldkey.currentState.showSnackBar(snackbar);
           },
         ),
-        body: Container(
-          color: Colors.blue,
-          height: 200,
-          width: 200,
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(left: 40, top: 40),
-          padding: EdgeInsets.all(55),
-          child: Text(
-            "boring !! ",
-            style: TextStyle(backgroundColor: Colors.white, fontSize: 30),
-          ),
+        body: RaisedButton(
+          onPressed: () {
+            createalert(context);
+          },
+          child: Text("alert"),
         )
+        //Container(color: Colors.blue,height: 200,width: 200,alignment: Alignment.center,margin: EdgeInsets.only(left: 40, top: 40),padding: EdgeInsets.all(55),child: Text("boring !! ",style: TextStyle(backgroundColor: Colors.white, fontSize: 30),),)
         //Container(margin: EdgeInsets.all(16),child: Column(children: [ TextField(controller: editingController,keyboardType: TextInputType.phone,decoration: InputDecoration(hintText: "Enter Phone Number",suffixIcon: Icon(Icons.phone),border: OutlineInputBorder()),),RaisedButton(onPressed: () {print('' + editingController.text);},child: Text("login"),)],)),
         //Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [RaisedButton(onPressed: () {},color: Colors.green,child: Text("next"),),RaisedButton(onPressed: () {},color: Colors.green,child: Text("pre"),)],)
         //Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Icon(Icons.home),Icon(Icons.list), ],),
@@ -129,5 +124,27 @@ class Page extends StatelessWidget {
         //Icon(Icons.fingerprint,color: Colors.blue,size: 50.0,)
         //Text("SIST",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blue,fontStyle: FontStyle.italic),)
         );
+  }
+
+  createalert(BuildContext context) {
+    var alertdialog = AlertDialog(
+      backgroundColor: Colors.blue,
+      elevation: 10.0,
+      title: Text("Congrats"),
+      content: Text("you have won iphone 7 plus"),
+      actions: [
+        GestureDetector(
+          child: Text("close"),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        )
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertdialog;
+        });
   }
 }
